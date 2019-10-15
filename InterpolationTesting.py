1ee4eb0df53ebcc8a733fececc12181e2655f11a
OrigamiAztec <origamiaztec@gmail.com>
2019-10-15 09:14:31 -0500

    Seeing if comment was added

---
+++ InterpolationTesting.py
@@ -0,0 +1,8 @@
+#Antonio Diaz, TAMU, antonio.diaz.hsa@tamu.edu, Oct 15, 2019
+#Testing out interpolation with polynomials
+#Ctrl alt N to run code in Visual Studio Code
+
+#Running example from http://www1.maths.leeds.ac.uk/~kersale/2600/Notes/appendix_E.pdf
+X = [1, 1.3, ]
+#adding comment right here to see if it updates
+Y = [3,4,5]
